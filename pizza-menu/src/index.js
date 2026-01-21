bddb9cbefe3710599060f7d6816b98f236ae0819
BharathBanti <dasarib180@gmail.com>
2026-01-21 16:57:07 +0530

    learned about conditional rendering & multiple return

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -69,31 +69,46 @@ function Header() {
 
 /* ----- Menu ----- */
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+  // const numPizzas = 0;
+
   return (
     <main className="menu">
-      <h2 style={{alignSelf: "center"}}>Our Menu</h2>
-      <Pizza name='Pizza Spinaci' ingredients="Tomato, mozarella, spinach, and ricotta cheese" photoName="pizzas/spinaci.jpg" price={10} />
+      <h2>Our Menu</h2>
+
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => {
+              return <Pizza pizzaObj={pizza} key={pizza.name} />
+            })}
+          </ul>
+        </React.Fragment>
+      ) : <p>We're still working on our menu. Please come back later :)</p>}
+
+      {/* <Pizza name='Pizza Spinaci' ingredients="Tomato, mozarella, spinach, and ricotta cheese" photoName="pizzas/spinaci.jpg" price={10} />
       <Pizza name='Pizza Focaccia' ingredients="Bread with italian olive oil and rosemary" photoName="pizzas/Focaccia.jpg" price={6} />
       <Pizza name='Pizza Margherita' ingredients="Tomato and mozarella" photoName="pizzas/margherita.jpg" price={12} />
       <Pizza name='Pizza Funghi' ingredients="Tomato, mozarella, mushrooms, and onion" photoName="pizzas/Funghi.jpg" price={12} />
       <Pizza name='Pizza Salamino' ingredients="Tomato, mozarella, and pepperoni" photoName="pizzas/Salamino.jpg" price={15} />
-      <Pizza name='Pizza Prosciutto' ingredients="Tomato, mozarella, ham, aragula, and burrata cheese" photoName="pizzas/Prosciutto.jpg" price={18} />
+      <Pizza name='Pizza Prosciutto' ingredients="Tomato, mozarella, ham, aragula, and burrata cheese" photoName="pizzas/Prosciutto.jpg" price={18} /> */}
     </main>
   )
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({ pizzaObj }) {
 
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>${props.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : `$${pizzaObj.price}`}</span>
       </div>
-    </div>
+    </li>
   )
 }
 
@@ -108,7 +123,22 @@ function Footer() {
   // if(hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry! we're closed");
 
-  return <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+  return (
+    <footer className="footer">
+      {isOpen ? <Order closeHour={closeHour} openHour={openHour} /> : (
+        <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
+      )}
+    </footer >
+  )
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
