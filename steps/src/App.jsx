ac705418901d95f52793eb6090357e786536b110
BharathBanti <dasarib180@gmail.com>
2026-01-22 11:18:22 +0530

    Simple practice for React State

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const messages = [
@@ -6,23 +7,57 @@ const messages = [
   "Invest your new income 💸"
 ];
 
-function App() {
-  const step = 1;
-
-  return <div className='steps'>
-    <div className='numbers'>
-      <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-      <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-      <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-    </div>
-
-    <p className='message'>Step {step}: {messages[step-1]}</p>
-
-    <div className='buttons'>
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={() => alert("previous")}>Previous</button>
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={() => alert("next")}>Next</button>
-    </div>
+function App(){
+  return <div>
+    <Steps />
+    <Steps />
   </div>
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  // const [test, setTest] = useState({name: "Banti"});
+
+  function handlePrevious(){
+    if(step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext(){
+    if(step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    };
+
+    // BAD PRACTICE
+    // test.name = "Bharath";
+    // step = step + 1;
+
+    // setTest({name: "Bharath"});
+  }
+
+  return (
+    <div className='steps-wrapper'>
+      <button className='close' onClick={() => setIsOpen((is) => !is)}>&times;</button>
+      {isOpen && (
+        <div className='steps'>
+          <div className='numbers'>
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className='message'>Step {step}: {messages[step-1]}</p>
+
+          <div className='buttons'>
+            <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous</button>
+            <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App
