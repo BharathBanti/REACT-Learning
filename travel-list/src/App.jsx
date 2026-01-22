deb1b15fea471efd137f00a47b2b7b708f83ea5d
BharathBanti <dasarib180@gmail.com>
2026-01-22 12:39:56 +0530

    Mini Project on React State

---
+++ travel-list/src/App.jsx
@@ -0,0 +1,79 @@
+// import { useState } from 'react'
+import './App.css'
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
+function App() {
+  return <div className='app'>
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
+}
+
+function Logo(){
+  return <h1>✈️🚂 Far Away 🏍️🚗</h1>;
+}
+
+function Form(){
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className='add-form' onSubmit={handleSubmit}>
+      <h3>What do you need for your 🌴 trip?</h3>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).
+        map(num =>(
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+
+        {/* <option value={1}>1</option>
+        <option value={1}>1</option>
+        <option value={1}>1</option> */}
+      </select>
+      <input type="text" placeholder='Item...' />
+      <button type='submit'>Add</button>
+    </form>
+  );
+}
+
+function PackingList(){
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map(item => <Item item={item}/>)}
+      </ul>
+    </div>
+  );
+}
+
+function Item({item}){
+  return (
+    <li className='listItem' key={item.id}>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.quantity}
+        {" "}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats(){
+  return <footer className='stats'>
+    <em>You have X items on your list, and you already packed X (X%)</em>
+  </footer>
+}
+
+export default App
